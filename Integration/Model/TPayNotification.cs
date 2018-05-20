e3860aa07860aaa2a1f4c6450245cce3c6c7190b
arst <artem.startsev@gmail.com>
2018-05-20 13:45:58 +0300

    Namespaces anjustement

---
+++ Integration/Model/TPayNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nop.Plugin.Payments.TPay.Integration.Model
+{
+    public class TPayNotification
+    {
+        public int Id { get; set; }
+        public string TranId { get; set; }
+        public DateTime TrDate { get; set; }
+        public string TrCrc { get; set; }
+        public string TrPaid { get; set; }
+        public string TrDesc { get; set; }
+        public string TrStatus { get; set; }
+        public string TrError { get; set; }
+        public string TrEmail { get; set; }
+        public string Md5Sum { get; set; }
+        public int TestMode { get; set; }
+        public string Wallet { get; set; }
+    }
+}
